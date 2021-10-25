4f3a9996ad385281d758114a326a759c9a990418
shawna5 <90216847+shawna5@users.noreply.github.com>
2021-10-25 17:20:17 -0400

    forecast javascript updated

--- src/index.js
+++ src/index.js
@@ -21,6 +21,36 @@ let minute = ("0" + now.getMinutes()).slice(-2);
 let currentTime = document.querySelector("#current-time");
 currentTime.innerHTML = `${day} ${hour}:${minute}`;
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 function search(event) {
   event.preventDefault();
   searchCity = document.querySelector("#example-input-city");
@@ -87,3 +117,5 @@ function convertCelsius(event) {
 }
 let celsiusClick = document.querySelector("#celsius-link");
 celsiusClick.addEventListener("click", convertCelsius);
+
+displayForecast();
